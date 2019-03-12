de744477303c48910151e084593bdc04d655d6d4
WaysideTester <dylanmurray211@outlook.com>
2019-03-11 21:23:39 -0500

    confusing project. The relationship key is used first so cannot be reused

--- FamilyDictionary/Program.cs
+++ FamilyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FamilyDictionary
 {
@@ -6,7 +7,14 @@ namespace FamilyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Dictionary<string, string>> MyFamily = new Dictionary<string, Dictionary<string, string>>();
+            MyFamily.Add("sister", new Dictionary<string, string>() { { "name", "Anastasia" }, { "age", "19" } });
+
+            foreach (var (key, value) in MyFamily)
+            {
+                Console.WriteLine($"{MyFamily[key]["name"]} is my {key} and is {MyFamily[key]["age"]} years old.");
+            }
+            Console.ReadKey();
         }
     }
 }
